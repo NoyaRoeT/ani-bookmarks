57a811f84ee5deb28727b1b1df4f382dbaabe000
NoyaReoT <rayonteo@gmail.com>
2023-05-16 17:16:43 +0800

    Redesign BookmarkCard

--- client/src/components/Bookmarks/BookmarkCard/BookmarkCard.jsx
+++ client/src/components/Bookmarks/BookmarkCard/BookmarkCard.jsx
@@ -23,6 +23,7 @@ const BookmarkCard = ({ bookmark }) => {
 			sm={6}
 			md={4}
 			lg={3}
+			xl={12 / 5}
 		>
 			<Card
 				sx={{
@@ -32,23 +33,47 @@ const BookmarkCard = ({ bookmark }) => {
 						zIndex: 100,
 					},
 					borderRadius: 2,
-					maxWidth: 250,
+					maxWidth: 220,
+					position: "relative",
+					width: 1,
 				}}
 			>
-				<CardActionArea>
-					<CardMedia
-						sx={{ height: 300 }}
-						image="https://via.placeholder.com/400"
-					/>
-					<CardContent>
-						<Typography gutterBottom variant="body2">
-							Manga
-						</Typography>
-						<Typography noWrap variant="h6">
-							Tensei Shitara Slime Datta Ken
-						</Typography>
-					</CardContent>
-				</CardActionArea>
+				<CardMedia
+					sx={{ height: 300 }}
+					image="https://via.placeholder.com/400"
+				/>
+				<CardContent
+					sx={{
+						zIndex: 2,
+						position: "absolute",
+						bottom: 0,
+						left: 0,
+						right: 0,
+						bgcolor: "grey.700",
+						px: "24px",
+						"&:last-child": {
+							pb: "12px",
+						},
+					}}
+				>
+					<Typography
+						sx={{
+							color: "#f0f0f0",
+							textTransform: "uppercase",
+							letterSpacing: "2",
+						}}
+						variant="body2"
+					>
+						Manga
+					</Typography>
+					<Typography
+						sx={{ color: "#f9d3b4", fontSize: "18px" }}
+						noWrap
+						variant="h6"
+					>
+						Tensei Shitara Slime Datta Ken
+					</Typography>
+				</CardContent>
 			</Card>
 		</Grid>
 	);
