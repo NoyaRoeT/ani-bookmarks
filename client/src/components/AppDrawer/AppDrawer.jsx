31f0fb2eb5cfb1ba911d751dabf9da7c55db072a
NoyaReoT <rayonteo@gmail.com>
2023-06-18 17:11:01 +0800

    Complete AppDrawer skeleton (without links)

--- client/src/components/AppDrawer/AppDrawer.jsx
+++ client/src/components/AppDrawer/AppDrawer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
 	Drawer,
@@ -7,13 +7,169 @@ import {
 	Toolbar,
 	IconButton,
 	useTheme,
+	Typography,
+	Avatar,
+	List,
+	ListSubheader,
+	ListItemButton,
+	ListItemText,
+	ListItemIcon,
+	Button,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import BookmarksIcon from "@mui/icons-material/Bookmarks";
+import SearchIcon from "@mui/icons-material/Search";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ArchiveIcon from "@mui/icons-material/Archive";
+import LogoutIcon from "@mui/icons-material/Logout";
+
+import logo from "../../assets/logo.jpg";
 
 const drawerWidth = 240;
 
+const ListIcon = ({ children }) => {
+	return <ListItemIcon sx={{ minWidth: "40px" }}>{children}</ListItemIcon>;
+};
+const DrawerContent = () => {
+	const theme = useTheme();
+	return (
+		<Box
+			sx={{
+				mx: 1,
+				display: "flex",
+				flexDirection: "column",
+				minHeight: "100vh",
+			}}
+		>
+			<Box sx={{ display: "flex", alignItems: "center", my: 1 }}>
+				<img src={logo} width={"48px"} />
+				<Typography
+					variant="h1"
+					fontSize="1.25rem"
+					lineHeight="1.75rem"
+					fontWeight={600}
+				>
+					Storybook
+				</Typography>
+			</Box>
+			<Box
+				sx={{
+					display: "flex",
+					alignItems: "center",
+					flexDirection: "column",
+					width: 1,
+					my: 3,
+				}}
+			>
+				<Avatar sx={{ mb: 1 }} alt="avatar">
+					N
+				</Avatar>
+				<Typography variant="h6">Username</Typography>
+			</Box>
+
+			<List
+				sx={{ mb: 3 }}
+				subheader={
+					<ListSubheader>
+						<Typography variant="subtitle2">BOOKMARKS</Typography>
+					</ListSubheader>
+				}
+			>
+				<ListItemButton>
+					<ListIcon>
+						<BookmarksIcon />
+					</ListIcon>
+					<ListItemText primary="Home" />
+				</ListItemButton>
+
+				<ListItemButton>
+					<ListIcon>
+						<SearchIcon />
+					</ListIcon>
+					<ListItemText primary="Search" />
+				</ListItemButton>
+
+				<ListItemButton>
+					<ListIcon>
+						<FavoriteIcon />
+					</ListIcon>
+					<ListItemText primary="Favorite" />
+				</ListItemButton>
+				<ListItemButton>
+					<ListIcon>
+						<ArchiveIcon />
+					</ListIcon>
+					<ListItemText primary="Archive" />
+				</ListItemButton>
+			</List>
+			<List
+				sx={{ mb: 3, flexGrow: 1 }}
+				subheader={
+					<ListSubheader>
+						<Typography variant="subtitle2">
+							USEFUL LINKS
+						</Typography>
+					</ListSubheader>
+				}
+			>
+				<ListItemButton>
+					<ListIcon>
+						<img
+							width="24px"
+							height="24px"
+							src="https://myanimelist.net/favicon.ico"
+						></img>
+					</ListIcon>
+					<ListItemText primary="MyAnimeList" />
+				</ListItemButton>
+				<ListItemButton>
+					<ListIcon>
+						<img
+							width="24px"
+							height="24px"
+							src="https://forum.novelupdates.com/favicon.ico"
+						/>
+					</ListIcon>
+					<ListItemText primary="NovelUpdates" />
+				</ListItemButton>
+				<ListItemButton>
+					<ListIcon>
+						<img
+							width="24px"
+							height="24px"
+							src="https://webtoons-static.pstatic.net/image/favicon/favicon.ico?dt=2017082301"
+						/>
+					</ListIcon>
+					<ListItemText primary="WebToons" />
+				</ListItemButton>
+			</List>
+			<Button
+				variant="contained"
+				sx={{
+					display: "flex",
+					justifyContent: "center",
+					width: 1,
+
+					py: "12px",
+					mb: 3,
+				}}
+			>
+				<LogoutIcon sx={{ mr: 1 }} />
+				<Typography>Logout</Typography>
+			</Button>
+		</Box>
+	);
+};
+
 const AppDrawer = () => {
 	const theme = useTheme();
+
+	const [isOpen, setIsOpen] = useState(false);
+
+	function handleDrawerToggle() {
+		setIsOpen((prev) => !prev);
+	}
+
 	return (
 		<>
 			<AppBar
@@ -21,6 +177,7 @@ const AppDrawer = () => {
 				sx={{
 					display: { xs: "block", sm: "none" },
 					bgcolor: theme.palette.background.default,
+					color: theme.palette.primary.main,
 				}}
 			>
 				<Toolbar>
@@ -28,17 +185,19 @@ const AppDrawer = () => {
 						color="inherit"
 						aria-label="open drawer"
 						edge="start"
+						onClick={handleDrawerToggle}
 					>
 						<MenuIcon />
 					</IconButton>
-					Responsive drawer
+					Responsive Drawer
 				</Toolbar>
 			</AppBar>
 			<Box component="nav" sx={{ width: { drawerWidth } }}>
 				{/* Temporary drawer for mobile */}
 				<Drawer
 					variant="temporary"
-					open={false}
+					open={isOpen}
+					onClose={handleDrawerToggle}
 					ModalProps={{
 						keepMounted: true, // Better open performance on mobile
 					}}
@@ -49,7 +208,9 @@ const AppDrawer = () => {
 							width: drawerWidth,
 						},
 					}}
-				></Drawer>
+				>
+					<DrawerContent />
+				</Drawer>
 
 				{/* Permanent drawer for larger screens */}
 				<Drawer
@@ -63,7 +224,7 @@ const AppDrawer = () => {
 						},
 					}}
 				>
-					Hello
+					<DrawerContent />
 				</Drawer>
 			</Box>
 		</>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
-
 import { createTheme, ThemeProvider } from "@mui/material";
 
-const theme = createTheme();
+const theme = createTheme({
+	palette: {
+		background: {
+			default: "rgb(250, 251, 251)",
+		},
+	},
+	typography: {
+		button: {
+			textTransform: "none",
+		},
+	},
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
