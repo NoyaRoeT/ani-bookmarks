80305f1b4784b2a4775d88eaaa58e36aa448a7bb
NoyaReoT <rayonteo@gmail.com>
2023-06-19 15:43:31 +0800

    Make Page component's margins responsive

--- client/src/components/Page/Page.jsx
+++ client/src/components/Page/Page.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import { Box } from "@mui/material";
 
 const Page = ({ children }) => {
-	return <Box sx={{ ml: "240px" }}>{children}</Box>;
+	return (
+		<Box sx={{ ml: { xs: 0, sm: "240px" }, mt: { xs: "64px", sm: 0 } }}>
+			{children}
+		</Box>
+	);
 };
 
 export default Page;
--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Paper, Toolbar } from "@mui/material";
 import { Page } from "../components";
 const Search = () => {
-	return <Page>Search</Page>;
+	return (
+		<Page>
+			<Paper elevation={0}>
+				<Toolbar>Yooo</Toolbar>
+			</Paper>
+		</Page>
+	);
 };
 
 export default Search;
